06be650aa0d77bba4686293593bf46459e7f294d
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2024-04-08 12:33:39 +0900

    feat: update cors policy to enable calguksu.com

--- src/main/kotlin/org/hufsdevelopers/calguksu/filter/CORSFilter.kt
+++ src/main/kotlin/org/hufsdevelopers/calguksu/filter/CORSFilter.kt
@@ -1,5 +1,3 @@
-package org.hufsdevelopers.calguksu.filter
-
 import jakarta.servlet.*
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -10,26 +8,35 @@ import java.io.IOException
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-class CORSFilter : Filter {
-    @Throws(ServletException::class)
-    override fun init(filterConfig: FilterConfig) {
+internal class CORSFilter : Filter {
+    override fun init(filterConfig: FilterConfig?) {
     }
 
     @Throws(IOException::class, ServletException::class)
     override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
         val request = req as HttpServletRequest
         val response = res as HttpServletResponse
-        response.setHeader("Access-Control-Allow-Origin", "*")
-        response.setHeader("Access-Control-Allow-Credentials", "true")
-        response.setHeader("Access-Control-Allow-Methods", "*")
-        response.setHeader("Access-Control-Max-Age", "3600")
-        response.setHeader(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-        )
+
+        val origin = request.getHeader("Origin")
+
+        if ("http://calguksu.com" == origin || "https://calguksu.com" == origin || "http://www.calguksu.com" == origin || "https://www.calguksu.com" == origin) {
+            response.setHeader("Access-Control-Allow-Origin", origin)
+            response.setHeader("Access-Control-Allow-Credentials", "true")
+            response.setHeader("Access-Control-Allow-Methods", "*")
+            response.setHeader("Access-Control-Max-Age", "3600")
+            response.setHeader(
+                "Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+            )
+        }
+
         if ("OPTIONS".equals(request.method, ignoreCase = true)) {
             response.status = HttpServletResponse.SC_OK
-        } else chain.doFilter(req, res)
+        } else {
+            chain.doFilter(req, res)
+        }
     }
-    override fun destroy() {}
+
+    override fun destroy() {
+    }
 }
